a687496c1acea7c55606dbc8426a4dfb5c016395
Yovieee <inimemangyonas@gmail.com>
2026-04-16 16:10:50 +0700

    penyempurnaan about

--- src/scripts/pages/about/about-page.js
+++ src/scripts/pages/about/about-page.js
@@ -1,4 +1,4 @@
-import AboutPresenter from './about-presenter';
+import AboutPresenter from "./about-presenter";
 
 export default class AboutPage {
   async render() {
@@ -8,6 +8,8 @@ export default class AboutPage {
         <article class="card">
           <p>This application was built with a modern MVP architecture and features smooth view transitions using the experimental View Transition API.</p>
           <p>Built as part of the Dicoding Front-End Web Developer Expert course.</p>
+          <p>Made with ❤️ by Blasius Yonas Vikariandi</p>
+          <p>For more information, please contact me at <a href="mailto:blasius.yonas@gmail.com">blasius.yonas@gmail.com</a></p>
         </article>
       </section>
     `;
